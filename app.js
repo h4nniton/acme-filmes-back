7e21ca0de1ec6ed5bf65feb6b5f1f87a3f2d1d8b
Emily Crepaldi <emily.ccarvalho13@gmail.com>
2024-01-30 15:55:39 -0300

    nao finalizado

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-//Import das bibliotecas para criar a API
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+/* 
+    npm install express --save
+     é a biblioteca que vai gerenciar as requisições da 
+ 
+    npm install body-parser --save
+     é a biblioteca que vai manipular dados do corpo da requisição (POST, PUT)
+
+    npm install cors --save
+     é a biblioteca responsável pelas permissões (HEADER) de acesso das requisições
+  
+*/ 
+
+//import das bibliotecas para criar a API
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { nextTick } = require('process');
 
 
-//Criando um objeto para manipular as requisições da API  (como instância de objeto no JAVA)
-const app = express()
+//criando um objeto app para manipular as requisições da API
+const app = express();
 
-//request - Entrada de dados na API (recebe)
-//response - Saída(return) de dados na API (devolve)
 
-//pacotes no protocolo http - cabeçalho(header) - onde ficam as referências (de onde vem, pra onde, permissões)
-//                          - corpo(body)
+
+//funçaio para manipular as restrições da API (HEADER)
+app.use((request, response, next) =>{
+    //permite especificar quem poderá acessar a API ('*' = liberar acesso público, 'IP' = liberar acesso apenas para aquela máquina)
+    response.header('Access-Control-Allow-Origin', '*')
+
+
+    //Permite especificar como a API, será requisitada (GET, POST, PUT e DELETE)
+    response.header('Access-Control-Allow-Methods', 'GET')
+
+    //ativa as configurações de permissão no cors
+    app.use(cors());
+
+
+    next();
+})
--- modulo/filmes.js
+++ modulo/filmes.js
@@ -2,33 +2,103 @@ var filmes = {
     "filmes": [
       {
 		"id": 1,  
-        "nome": "O Segredo do Vale",
-        "sinopse": "Um drama emocionante que explora os segredos de uma pequena cidade no interior.",
-        "duracao": "2 horas e 15 minutos",
-        "data_lancamento": "2022-08-25",
+        "nome": "Paranorman",
+        "sinopse": "",
+        "duracao": "1 hora e 33 minutos",
+        "data_lancamento": "2022-09-07",
         "data_relancamento": null,
         "foto_capa": "https://exemplo.com/foto_vale.jpg",
         "valor_unitario": 29.99
       },
       {
 		"id": 2,  
-        "nome": "A Herança Perdida",
-        "sinopse": "Um filme de suspense que segue a jornada de um herdeiro em busca de seu passado misterioso.",
-        "duracao": "2 horas e 30 minutos",
-        "data_lancamento": "2023-03-12",
-        "data_relancamento": "2023-10-05",
+        "nome": "A sociedade da neve",
+        "sinopse": "",
+        "duracao": "2 horas e 24 minutos",
+        "data_lancamento": "2023-10-20",
+        "data_relancamento": null,
         "foto_capa": "https://exemplo.com/foto_heranca.jpg",
         "valor_unitario": 24.99
       },
       {
 		"id": 3,  
-        "nome": "A Viagem no Tempo",
-        "sinopse": "Uma aventura sci-fi que leva os espectadores a uma jornada através das eras.",
-        "duracao": "2 horas",
-        "data_lancamento": "2024-01-07",
+        "nome": "Nada de novo do front",
+        "sinopse": "",
+        "duracao": "2 horas e 27 minutos",
+        "data_lancamento": "2022-09-29",
         "data_relancamento": null,
         "foto_capa": "https://exemplo.com/foto_tempo.jpg",
         "valor_unitario": 34.99
-      }
+      },
+      {
+        "id": 4,
+        "nome": "Cisne negro",
+        "sinopse": "",
+        "duracao": "1 hora e 48 minutos",
+        "data_lancamento": "2010-02-04",
+        "data_relancamento": null,
+        "foro_capa": "https://exemplo.com/foto_tempo.jpg",
+        "valor_unitario": 29.99
+      },
+      {
+        "id": 5,
+        "nome": "A nova cinderela",
+        "sinopse": "",
+        "duracao": "1 hora e 35 minutos",
+        "data_lancamento": "2010-10-01",
+        "data_relancamento": null,
+        "foro_capa": "https://exemplo.com/foto_tempo.jpg",
+        "valor_unitario": 23.99
+      },
+      {
+        "id": 6,
+        "nome": "De repente 30",
+        "sinopse": "",
+        "duracao": "1 hora e 38 minutos",
+        "data_lancamento": "2004-08-23",
+        "data_relancamento": null,
+        "foro_capa": "https://exemplo.com/foto_tempo.jpg",
+        "valor_unitario": 29.99
+      },
+      {
+        "id": 7,
+        "nome": "10 coisas que eu odeio em você",
+        "sinopse": "",
+        "duracao": "1 hora e 37 minutos",
+        "data_lancamento": "1999-11-31",
+        "data_relancamento": null,
+        "foro_capa": "https://exemplo.com/foto_tempo.jpg",
+        "valor_unitario": 19.99
+      },
+      {
+        "id": 8,
+        "nome": "Sociedade dos poetas mortos",
+        "sinopse": "",
+        "duracao": "2 horas e 8 minutos",
+        "data_lancamento": "1989-02-28",
+        "data_relancamento": null,
+        "foro_capa": "https://exemplo.com/foto_tempo.jpg",
+        "valor_unitario": 49.99
+      },
+      {
+        "id": 9,
+        "nome": "Antes do amanhecer",
+        "sinopse": "",
+        "duracao": "1 hora e 45 minutos",
+        "data_lancamento": "2995-01-27",
+        "data_relancamento": null,
+        "foro_capa": "https://exemplo.com/foto_tempo.jpg",
+        "valor_unitario": 22.99
+      },
+      {
+        "id": 10,
+        "nome": "Garotas malvadas",
+        "sinopse": "",
+        "duracao": "1 hora e 37 minutos",
+        "data_lancamento": "2004-07-09",
+        "data_relancamento": null,
+        "foro_capa": "https://exemplo.com/foto_tempo.jpg",
+        "valor_unitario": 34.99
+      },
     ]
   }
