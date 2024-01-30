0f7a100449850b3a371d4518a3c8b3ae4e748987
Emily Crepaldi <emily.ccarvalho13@gmail.com>
2024-01-30 14:10:14 -0300

    nao finalizado

--- controller/functions.js
+++ controller/functions.js
@@ -1,3 +1,18 @@
 var acmeFilmes = require('../modulo/filmes.js');
 
+const getListarFilmes = functions(){
+
+    let filmes = acmeFilmes.filmes.filmes
+
+    filmJson = {}
+    filmArray = []
+
+    filmJson.filmes = filmArray
+    
+    filmes.forEach(function(filmes){
+        filmArray.push(filmes)
+    })
+
+    return filmJson
+}
 
