fc4e4782d3786e0df7fbbe5b2294e66c0bba1391
Emily Crepaldi <emily.ccarvalho13@gmail.com>
2024-01-30 17:20:23 -0300

    nao finalizado

---
+++ controller/controller_filme.js
@@ -0,0 +1,42 @@
+/*****************************************************************************************
+ * Objetivo: Arquivo responsável por realizar validações, consistências e regra de negócio
+ *     para os filmes
+ * Data: 30/01/2024
+ * Autor: Emily Crepaldi
+ * Versão: 1.0
+ * 
+ *****************************************************************************************/
+
+// função para inserir um novo filme
+const setInserirNovoFilme = async function () {
+
+}
+
+// função para atualizar um Filme existente
+const setAtualizarFilme = async function () {
+
+}
+
+// função para excluir um Filme existente
+const setExcluirFilme = async function () {
+
+}
+
+// função para retornar todos os Filme dos banco de dados
+const getListarFilmes = async function () {
+
+}
+
+// função para retornar o filtro de um Filme pelo ID
+const getBuscarFilme = async function () {
+
+}
+
+
+module.exports = {
+    setInserirNovoFilme,
+    setAtualizarFilme,
+    setExcluirFilme,
+    getListarFilmes,
+    getListarFilmes
+}
---
+++ modulo/DAO/filme.js
@@ -0,0 +1,41 @@
+/*****************************************************************************************
+ * Objetivo: Arquivo responsável por realizar o CRUD no Banco de Dados MySQL
+ * Data: 30/01/2024
+ * Autor: Emily Crepaldi
+ * Versão: 1.0
+ * 
+ *****************************************************************************************/
+
+// função para inserir um Filme no Banco de Dados
+const insertFilme = async function () {
+
+}
+
+// função para atualizar um Filme no Banco de Dados
+const updateFilme = async function () {
+
+}
+
+// função para excluir um Filme no Banco de Dados
+const deleteFilme = async function () {
+
+}
+
+// função para retornar um Filme no Banco de Dados
+const selectAllFilmes = async function () {
+
+}
+
+// função para buscar um Filme no Banco de Dados filtrando pelo ID
+const selectById = async function () {
+
+}
+
+
+module.exports = {
+    insertFilme,
+    updateFilme,
+    deleteFilme,
+    selectAllFilmes,
+    selectById
+}
